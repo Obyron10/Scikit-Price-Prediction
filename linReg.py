7d01b24da60165f460ede0e55e69f573160d07d6
IgorShest31 <owenbyron1@gmail.com>
2025-07-31 17:49:27 -0400

    Create linReg.py

---
+++ linReg.py
@@ -0,0 +1,11 @@
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import LearningCurveDisplay
+import pandas as pd
+import numpy as np
+
+
+#Predict house prices using linear regression, based of # of rooms,bathrooms, square footage, and zipcode
+
+#load data
